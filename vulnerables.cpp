129c5b452d4d7233ab088b34ad5369c651d54cf3
Abhishek Singh <abhisheksingh@uptycs.com>
2023-04-26 15:07:06 +0530

    Removed the file reading for deb packages now getting packages data directly from deb package table

--- vulnerables.cpp
+++ vulnerables.cpp
@@ -1,61 +1,52 @@
 #include <fstream>
 #include <iostream>
-#include <osquery/core/tables.h>
 #include <sstream>
 #include <unordered_map>
+#include </home/adhisheksingh/osquery/osquery/sql/sql.h>
 
 using namespace std;
 
-namespace osquery {
-namespace tables {
+namespace osquery
+{
+    namespace tables
+    {
 
-QueryData genVulnerablePackages(QueryContext& context) {
-  QueryData rows;
-  auto paths = context.constraints["path"].getAll(EQUALS);
-  auto it = paths.begin();
-  string path = *it;
-  cout<<" 1 "<<path<<" 2 ";
-  unordered_map<string, string> packages;
-  string line, version, name;
-  string packageName, packageVersion, first, second;
+        QueryData genVulnerablePackages(QueryContext &context)
+        {
+            QueryData rows;
+            auto paths = context.constraints["path"].getAll(EQUALS);
+            auto it = paths.begin();
+            string path = *it;
+            unordered_map<string, string> packages;
+            string line, version, name;
+            string packageName, packageVersion, first, second;
 
-  ifstream vulnerableFile(
-      path);
-  if (vulnerableFile.is_open()) {
-    while (vulnerableFile) {
-      getline(vulnerableFile, line);
-      istringstream iss(line);
-      iss >> name >> version;
-      packages[name] = version;
-    }
-  }
-  vulnerableFile.close();
-  ifstream packagesFile("/var/lib/dpkg/status");
-  if (packagesFile.is_open()) {
-    while (packagesFile) {
-      getline(packagesFile, line);
-      if (!line.empty()) {
-        istringstream iss(line);
-        iss >> first >> second;
-        if (first == "Package:") {
-          packageName = second;
-        } else if (first == "Version:") {
-          packageVersion = second;
+            ifstream vulnerableFile(path);
+            if (vulnerableFile.is_open())
+            {
+                while (vulnerableFile)
+                {
+                    getline(vulnerableFile, line);
+                    istringstream iss(line);
+                    iss >> name >> version;
+                    packages[name] = version;
+                }
+            }
+            vulnerableFile.close();
+
+            QueryData deb_packages = SQL::selectAllFrom("deb_packages");
+            for (const auto &package : deb_packages)
+            {
+                if (packages[package.at("name")] == package.at("version"))
+                {
+                    Row r;
+                    r["package_name"] = package.at("name");
+                    r["package_version"] = package.at("version");
+                    r["path"] = path;
+                    rows.push_back(r);
+                }
+            }
+            return rows;
         }
-      } else {
-        if (packages[packageName] == packageVersion) {
-          Row r;
-          r["package_name"] = packageName;
-          r["package_version"] = packageVersion;
-          r["path"] = path;
-          rows.push_back(std::move(r));
-        }
-      }
     }
-    packagesFile.close();
-  }
-  cout<<"done";
-  return rows;
 }
-} // namespace tables
-} // namespace osquery
